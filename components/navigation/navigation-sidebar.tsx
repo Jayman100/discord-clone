765e811d63992715630a50648099c70022b011f3
Garba jamiu <garbajamiu2000@gmail.com>
2024-08-12 16:42:56 +0100

    feat: display sidebar and it items

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,7 +1,13 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
-// import Image from "next/image";
+import NavigationAction from "./navigation-action";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import NavigationItems from "./navigation-items";
+import { ModeToggle } from "../mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+import ActionTooptip from "../action-tooltip";
 
 const NavigationSidebar = async () => {
   const profile: any = await currentProfile();
@@ -20,7 +26,29 @@ const NavigationSidebar = async () => {
 
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
-      sidebar
+      <NavigationAction />
+
+      <Separator className="h-[12px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItems
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSwitchSessionUrl="/"
+          appearance={{ elements: { avatarBox: "h-[48px] w-[48px]" } }}
+        />
+      </div>
     </div>
   );
 };
