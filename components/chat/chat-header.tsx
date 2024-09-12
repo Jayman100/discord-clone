4d9dbb7508ddf3529a05f72b708e0078d9cd94f1
Garba jamiu <garbajamiu2000@gmail.com>
2024-09-12 18:44:06 +0100

    implement mobile navigation menu and complete the db schema

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
-import { Hash, Menu } from "lucide-react";
+import { Hash } from "lucide-react";
+import MobileToggle from "../mobile.-toggle";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -10,7 +11,7 @@ interface ChatHeaderProps {
 const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-      <Menu />
+      <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="h-5 w-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
