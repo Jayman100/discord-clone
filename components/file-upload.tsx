f52da5d5a7398a1c68dc473f049dd6095675530d
Garba jamiu <garbajamiu2000@gmail.com>
2024-09-30 13:35:58 +0100

    feat: implement message  file upload

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -3,7 +3,7 @@
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 interface FileUploadProps {
@@ -30,6 +30,29 @@ const FileUpload = ({ endpoint, onChange, value }: FileUploadProps) => {
     );
   }
 
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center pt-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
@@ -37,7 +60,7 @@ const FileUpload = ({ endpoint, onChange, value }: FileUploadProps) => {
         onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
-        console.error(error);
+        console.error(error, "file Error");
       }}
     />
   );
